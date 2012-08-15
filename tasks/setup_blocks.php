8b95c6290fdb9631d82f8a0aaaea8a6aeb189643
Jon Duell <jon@jonduell.com>
2012-08-15 16:24:36 -0700

    Fixes blocks being assigned to missing regions (dashboard)

--- tasks/setup_blocks.php
+++ tasks/setup_blocks.php
@@ -67,16 +67,6 @@ function ombuprofile_setup_blocks($install_state) {
   // New module blocks to insert.
   $new_blocks = array(
     array(
-      'module' => 'node',
-      'delta' => 'recent',
-      'theme' => $admin_theme,
-      'status' => 1,
-      'weight' => 10,
-      'region' => 'dashboard_main',
-      'pages' => '',
-      'cache' => -1,
-    ),
-    array(
       'module' => 'system',
       'delta' => 'main',
       'theme' => $admin_theme,
@@ -106,33 +96,13 @@ function ombuprofile_setup_blocks($install_state) {
       'pages' => '',
       'cache' => -1,
     ),
-    array(
-      'module' => 'user',
-      'delta' => 'new',
-      'theme' => $admin_theme,
-      'status' => 1,
-      'weight' => 0,
-      'region' => 'dashboard_sidebar',
-      'pages' => '',
-      'cache' => -1,
-    ),
-    array(
-      'module' => 'search',
-      'delta' => 'form',
-      'theme' => $admin_theme,
-      'status' => 1,
-      'weight' => -10,
-      'region' => 'dashboard_sidebar',
-      'pages' => '',
-      'cache' => -1,
-    ),
   );
   $query = db_insert('block')->fields(array('module', 'delta', 'theme', 'status', 'weight', 'region', 'pages', 'cache'));
   foreach ($new_blocks as $record) {
     $query->values($record);
   }
   $query->execute();
-  
+
   // Create new blocks
   $delta = db_insert('block_custom')
     ->fields(array(
